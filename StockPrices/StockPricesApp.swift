2c35db6c5cb936cf59928955755525d1503495a4
Karl McPhee <karmcphee@cox.net>
2023-11-17 23:48:03 -0500

    Initial Commit

---
+++ StockPrices/StockPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPricesApp.swift
+//  StockPrices
+//
+//  Created by Karl McPhee on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct StockPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
